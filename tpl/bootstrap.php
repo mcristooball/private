d16c7f548d16aa37a3df8653800ab3f892bc8977
Miguel <miguel@dreamsengine.io>
2021-02-04 21:46:14 -0600

    Bl

--- tpl/bootstrap.php
+++ tpl/bootstrap.php
@@ -575,7 +575,7 @@ endif;
 			</section>
 			<footer class="container">
 				<div class="row">
-					<h4 class="col-md-5 col-xs-8"><?php echo I18n::_($NAME); ?> <small>- <?php echo I18n::_('Because ignorance is bliss'); ?></small></h4>
+					<h4 class="col-md-5 col-xs-8"><?php echo I18n::_($NAME); ?> <small>- <?php echo I18n::_('😎'); ?></small></h4>
 					<p class="col-md-1 col-xs-4 text-center"><?php echo $VERSION; ?></p>
 					<p id="aboutbox" class="col-md-6 col-xs-12">
 						<?php echo I18n::_('%s is a minimalist, open source online pastebin where the server has zero knowledge of pasted data. Data is encrypted/decrypted using 256 bits AES.', I18n::_($NAME)), ' ', $INFO, PHP_EOL; ?>
